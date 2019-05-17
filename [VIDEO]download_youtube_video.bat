5c59c9c2c8b1a30f186d05fe8e65b47a99f9435b
Seth G. R. Herendeen <inivican@hotmail.com>
2019-05-17 13:57:18 -0400

    Update [VIDEO]download_youtube_video.bat

--- [VIDEO]download_youtube_video.bat
+++ [VIDEO]download_youtube_video.bat
@@ -1,4 +1,8 @@
 @echo off
+
+REM This script is in the Public Domain
+REM Originally made by Seth G. R. Herendeen
+
 echo Input link representing video to be downloaded
 set /p video=:
 youtube-dl -f mp4 %video%
